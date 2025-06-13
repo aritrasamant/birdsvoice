2a9c61a31a8999c3d36a22256127a4fe22375c26
ARITRA SAMANTA <119259648+aritrasamant@users.noreply.github.com>
2025-06-13 21:59:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -129,3 +129,8 @@ app.mount("/static", StaticFiles(directory=STATIC_FOLDER), name="static")
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+if __name__ == "__main__":
+    import uvicorn
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
